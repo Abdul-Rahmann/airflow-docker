7ab0c3955169a64073f1269fe179163d4ca88256
Samuel Adetsi <abdulrahmanadetsi@gmail.com>
2025-09-19 11:14:30 -0700

    create a dag with catchup feature

---
+++ dags/dag_with_catchup_and_backfill.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'sam',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    dag_id='dag_with_catchup_and_backfill',
+    default_args=default_args,
+    description='This is a bash operator dag',
+    start_date=datetime(2025, 9, 1),
+    schedule='@daily',
+    catchup=True,
+    # backfill=True
+) as dag:
+    task1 = BashOperator(
+        task_id='first_task',
+        bash_command='echo This is a simple bash command!'
+    )
+
+    task1
