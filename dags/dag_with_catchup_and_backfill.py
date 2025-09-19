a91bf569572096df5ce03e75a9f140ea12b46b55
Samuel Adetsi <abdulrahmanadetsi@gmail.com>
2025-09-19 11:35:39 -0700

    add catchup and backfill

--- dags/dag_with_catchup_and_backfill.py
+++ dags/dag_with_catchup_and_backfill.py
@@ -9,13 +9,12 @@ default_args = {
 }
 
 with DAG(
-    dag_id='dag_with_catchup_and_backfill',
+    dag_id='dag_with_catchup_and_backfill_v2',
     default_args=default_args,
     description='This is a bash operator dag',
     start_date=datetime(2025, 9, 1),
     schedule='@daily',
-    catchup=True,
-    # backfill=True
+    catchup=False
 ) as dag:
     task1 = BashOperator(
         task_id='first_task',
